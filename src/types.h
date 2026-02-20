3ffbaef15fec59858eb4dbc9b3254ff99cc44dde
Aria Horak <aweeri.eu@gmail.com>
2026-02-20 01:05:38 +0100

    cranked max satellite count for funsies

--- src/types.h
+++ src/types.h
@@ -8,7 +8,7 @@
 #include "csgp4.h"
 
 // basic limits and math constants
-#define MAX_SATELLITES 2000
+#define MAX_SATELLITES 15000
 #define MAX_MARKERS 100
 #define EARTH_RADIUS_KM 6371.0f 
 #define MOON_RADIUS_KM 1737.4f 
