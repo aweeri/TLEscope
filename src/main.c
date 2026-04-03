0b20787ca481def96658320daa44764d16899ada
aweeri <aweeri.eu@gmail.com>
2026-04-03 11:45:39 +0200

    bring back orbits. oops.

--- src/main.c
+++ src/main.c
@@ -1823,8 +1823,7 @@ int main(void)
                 bool is_hl = (active_sat == &satellites[i]);
                 if (!(is_pov_mode && &satellites[i] == selected_sat))
                 {
-                    if (is_hl)
-                        draw_orbit_3d(&satellites[i], current_epoch, is_hl, sat_alpha, global_orbit_step);
+                    draw_orbit_3d(&satellites[i], current_epoch, is_hl, sat_alpha, global_orbit_step);
                 }
 
                 if (is_hl && !(is_pov_mode && &satellites[i] == selected_sat))
